aab96cfc68169af0469966d08b1ea1d7b8969555
James Benoit <jrabenoit@gmail.com>
2017-08-13 15:42:31 -0600

    sun afternoon

--- prep.py
+++ prep.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import os
+import numpy as np
+import dask.dataframe as dd
 
 #Gives average accuracy of each param set tested on inner loop data
 def Framer():
     
-    df1=pd.read_csv('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csv/deid_adverse.csv')
-    df2=pd.read_csv('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csv/deid_hamd17.csv')
+    dfs=dd.read_csv('/media/james/ext4data1/current/projects/pfizer/deid_adverse.csv', encoding='utf-8')
+    c=1
+    
+    for i in os.listdir('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csv/'):
+        print('{}/45, {}'.format(c,i))
+        c=c+1
+        check= pd.DataFrame.from_csv('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csv/'+str(i), encoding='utf-8')
+        df1=dd.read_csv('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csv/'+str(i), encoding='utf-8')
+        if ('PATIENT' in check.keys() and 'CPENM' in check.keys()):
+            dfs=dd.merge(dfs, df1, how='outer', on=['PATIENT'])
+    
+    dfs= dfs.apply(pd.to_numeric, errors='coerce', axis=1)
 
-    cdf=pd.merge(df1, df2, how='outer', left_on=['PATIENT'], right_on=['CPENM'])
-    
-    cdf.to_csv('/media/james/ext4data1/current/projects/pfizer/cdf.csv')
-    
+    #dd.to_csv(dfs, '/media/james/ext4data1/current/projects/pfizer/cdf.csv')
+
     return
