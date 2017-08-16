aba30530be77bb8f1d33451bc5aad25dfe907b04
James Benoit <jrabenoit@gmail.com>
2017-08-15 20:37:58 -0600

    tues eve commit

--- prep.py
+++ prep.py
@@ -3,24 +3,25 @@
 import pandas as pd
 import os
 import numpy as np
-import dask.dataframe as dd
 
 #Gives average accuracy of each param set tested on inner loop data
-def Framer():
-    
-    dfs=dd.read_csv('/media/james/ext4data1/current/projects/pfizer/deid_adverse.csv', encoding='utf-8')
-    c=1
-    
+def Cutter():
+    for i in os.listdir('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csv2/'):    
+        df1=pd.read_csv('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csv2/'+str(i), encoding='utf-8')
+        if 'RELDAYU' in df1.keys(): df1= df1.drop('RELDAYU', 1)
+        
+        df1.to_csv('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csv2/'+str(i))    
+    return
+
+def Framer():    
+    dfs=pd.read_csv('/media/james/ext4data1/current/projects/pfizer/deid_adverse.csv', encoding='utf-8')
+    c=0
     for i in os.listdir('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csv/'):
-        print('{}/45, {}'.format(c,i))
         c=c+1
-        check= pd.DataFrame.from_csv('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csv/'+str(i), encoding='utf-8')
-        df1=dd.read_csv('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csv/'+str(i), encoding='utf-8')
-        if ('PATIENT' in check.keys() and 'CPENM' in check.keys()):
-            dfs=dd.merge(dfs, df1, how='outer', on=['PATIENT'])
-    
-    dfs= dfs.apply(pd.to_numeric, errors='coerce', axis=1)
-
-    #dd.to_csv(dfs, '/media/james/ext4data1/current/projects/pfizer/cdf.csv')
-
+        print('{}/45'.format(c))
+        df1=pd.read_csv('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csv/'+str(i), encoding='utf-8')
+        if ('PATIENT' in df1.keys() and 'CPENM' in df1.keys()):
+            dfs=pd.merge(dfs, df1, how='left', on=['PATIENT', 'CPENM'])
+            dfs=dfs.drop_duplicates()
+    pd.to_csv(dfs, '/media/james/ext4data1/current/projects/pfizer/cdf.csv')
     return
---
+++ prep_dd.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import os
+import numpy as np
+import dask.dataframe as dd
+
+#Gives average accuracy of each param set tested on inner loop data
+def Framer():
+    
+    dfs=dd.read_csv('/media/james/ext4data1/current/projects/pfizer/deid_adverse.csv', encoding='utf-8')
+    c=1
+    
+    for i in os.listdir('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csv/'):
+        print('{}/45, {}'.format(c,i))
+        c=c+1
+        check= pd.DataFrame.from_csv('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csv/'+str(i), encoding='utf-8')
+        df1=dd.read_csv('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csv/'+str(i), encoding='utf-8')
+        if ('PATIENT' in check.keys() and 'CPENM' in check.keys()):
+            dfs=dd.merge(dfs, df1, how='outer', on=['PATIENT'])
+
+    #dd.to_csv(dfs, '/media/james/ext4data1/current/projects/pfizer/cdf.csv')
+
+    return
