a6d8a589e0fca6e0e6a9f48d86efe0b492df495b
James Benoit <jrabenoit@gmail.com>
2017-09-09 22:03:11 -0600

    z

---
+++ crossval.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+
+import pprint, itertools, pickle
+import numpy as np
+import pandas as pd
+from collections import defaultdict
+from sklearn.feature_selection import SelectKBest
+from sklearn.model_selection import StratifiedKFold
+
+def CVSetup():   
+    
+    with open('/media/james/ext4data1/current/projects/pfizer/vectors/data.pickle', 'rb') as f:
+        data= pickle.load(f)
+    
+    print('Treatment Response:\n')
+    print(' 1= nonresponder/remitter \n 2= nonresponder/responder \n 3= nonresponder/(remitter+responder) \n 4= responder/remitter')
+    
+    print('\nStudy contains {} Subjects'.format(len(data.index)))
+    choice= int(input('\nChoice: '))
+        
+    group0=[]
+    group1=[]
+
+    elif choice== 1:
+        for i in data:
+            if data[i]['sx severity']==0: group0.append(i)
+            if data[i]['sx severity']==2: group1.append(i)
+    elif choice== 2:
+        for i in data:
+            if data[i]['sx severity']==0: group0.append(i)
+            if data[i]['sx severity']==3: group1.append(i)   
+    elif choice== 3:
+        for i in data:
+            if data[i]['sx severity']==1: group0.append(i)
+            if data[i]['sx severity']==2: group1.append(i)       
+    elif choice== 4:
+        for i in data:
+            if data[i]['sx severity']==1: group0.append(i)
+            if data[i]['sx severity']==3: group1.append(i)      
+    elif choice== 5:
+        for i in data:
+            if data[i]['sx severity']==2: group0.append(i)
+            if data[i]['sx severity']==3: group1.append(i)
+    elif choice== 6:
+        group1a=[]
+        group1b=[]
+        group1c=[]
+        for i in data:
+            if data[i]['sx severity']==0: group0.append(i)
+            if data[i]['sx severity']==1: group1a.append(i)
+            if data[i]['sx severity']==2: group1b.append(i)
+            if data[i]['sx severity']==3: group1c.append(i)
+            group1= np.sort(np.append(group1a,np.append(group1b,group1c)))   
+    elif choice== 7:
+        for i in data:
+            if data[i]['tx response']==0: group0.append(i)
+            if data[i]['tx response']==1: group1.append(i)
+    elif choice== 8:
+        group1a=[]
+        group1b=[]
+        for i in data:
+            if data[i]['tx response']==0: group0.append(i)
+            if data[i]['tx response']==2: group1a.append(i)
+            if data[i]['tx response']==3: group1a.append(i)
+            group1= np.sort(np.append(group1a,group1b))  
+    elif choice== 9:
+        for i in data:
+            if data[i]['tx response']==0: group0.append(i)
+            if data[i]['tx response']==3: group1.append(i)
+    elif choice== 10:
+        group1a=[]
+        group1b=[]
+        for i in data:
+            if data[i]['tx response']==1: group0.append(i)
+            if data[i]['tx response']==2: group1a.append(i)
+            if data[i]['tx response']==3: group1a.append(i)
+            group1= np.sort(np.append(group1a,group1b))      
+    elif choice== 11:
+        for i in data:
+            if data[i]['tx response']==1: group0.append(i)
+            if data[i]['tx response']==3: group1.append(i)
+    elif choice== 12:
+        for i in data:
+            if data[i]['tx response']==2: group0.append(i)
+            if data[i]['tx response']==3: group1.append(i)  
+    elif choice== 13:
+        group1a=[]
+        group1b=[]
+        group1c=[]
+        for i in data:
+            if data[i]['tx response']==0: group0.append(i)
+            if data[i]['tx response']==1: group1a.append(i)
+            if data[i]['tx response']==2: group1a.append(i)
+            if data[i]['tx response']==3: group1a.append(i)
+            group1= np.sort(np.append(group1a,np.append(group1b,group1c)))
+    else: print('Invalid selection.')
+    
+    group0.sort()
+    group1.sort()
+    
+    if len(group0)>len(group1): 
+        group0=np.random.choice(group0, size=len(group1), replace= False) 
+        labels= np.array([0]*len(group0)+[1]*len(group1)) 
+    elif len(group0)<len(group1): 
+        group1=np.random.choice(group1, size=len(group0), replace= False)
+        labels= np.array([0]*len(group0)+[1]*len(group1)) 
+    
+    #For group 0 and 1, take n random subjects in the larger group where n=n_subjects in smaller group.    
+    
+    outer_cv={'X_train': [], 'X_test': [],
+              'y_train': [], 'y_test': []}
+
+    X= np.append(group0,group1)
+    y= labels
+    
+    X_train, X_test, y_train, y_test= [], [], [], []      
+
+    skf = StratifiedKFold(n_splits=5)
+    
+    print('\nGroup size: {}, group0: {}, group1: {}'.format(len(X), len(group0), len(group1)))
+    print('Group indices: {}'.format(X))
+    print('\nLabel size: {}, group0: {}, group1: {}'.format(len(y), (y==0).sum(), (y==1).sum()))
+    print('Labels: {}\n'.format(y))
+        
+    for train_index, test_index in skf.split(X,y):
+        #print("TRAIN:", train_index, "\nTEST:", test_index)
+        X_train, X_test= X[train_index], X[test_index]
+        y_train, y_test= y[train_index], y[test_index]
+    
+        outer_cv['X_train'].append(X_train)
+        outer_cv['X_test'].append(X_test)
+        outer_cv['y_train'].append(y_train)
+        outer_cv['y_test'].append(y_test)
+
+    with open('/media/james/ext4data1/current/projects/pfizer/vectors/outer_cv.pickle', 'wb') as f: pickle.dump(outer_cv, f, pickle.HIGHEST_PROTOCOL) 
+
+    return
+    
+def InnerCv():
+    '''Set up as a flat structure of 10 df'''
+    with open('/media/james/ext4data1/current/projects/pfizer/vectors/outer_cv.pickle', 'rb') as f: outer_cv= pickle.load(f)
+
+    inner_cv={'X_train': [], 'X_test': [],
+              'y_train': [], 'y_test': []}
+    
+    X= outer_cv['X_train']
+    y= outer_cv['y_train']
+    
+    X_train, X_test, y_train, y_test = [], [], [], []
+    
+    #change X to subjects, y to labels
+    #read loop as, "for each pair of X and y lists in (X,y)"
+    
+    for X_, y_ in zip(X, y): 
+        skf = StratifiedKFold(n_splits=2)
+        for train_index, test_index in skf.split(X_,y_):      
+            X_train, X_test= X_[train_index], X_[test_index]
+            y_train, y_test= y_[train_index], y_[test_index]
+
+            inner_cv['X_train'].append(X_train)
+            inner_cv['X_test'].append(X_test)
+            inner_cv['y_train'].append(y_train)
+            inner_cv['y_test'].append(y_test)
+
+    with open('/media/james/ext4data1/current/projects/pfizer/vectors/inner_cv.pickle', 'wb') as f:
+        pickle.dump(inner_cv, f, pickle.HIGHEST_PROTOCOL) 
+    
+    return
+
+    
--- pivoter.py
+++ pivoter.py
@@ -1,25 +1,66 @@
 #!/usr/bin/env python3
 
 import pandas as pd
-import os
 import numpy as np
+import os, pickle
 
 #Gives average accuracy of each param set tested on inner loop data
 def Pivot():
-    dfs=pd.read_csv('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csv2/deid_hamd17.csv')
+    df=pd.read_csv('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csv2/deid_hamd17.csv')
 
-    dfs2=dfs[dfs['CPENM']=='DAY 7']
-    dfs3=dfs[dfs['CPENM']=='DAY 300']
+    df2=df[df['CPENM']=='DAY 7']
+    df3=df[df['CPENM']=='DAY 300']
     
-    dfs2b= dfs2.pivot(index='PATIENT',columns='TESTS',values='VALN')
-    dfs3b= dfs3.pivot(index='PATIENT',columns='TESTS',values='VALN')
-    
-    dfs2b['HAMD Total'].describe()
+    df2b= df2.pivot(index='PATIENT',columns='TESTS',values='VALN')
+    df3b= df3.pivot(index='PATIENT',columns='TESTS',values='VALN')
 
+    df2b.columns='Day 7: ' + df2b.columns
+    df3b.columns='Day 300: ' + df3b.columns
+    
+    dfc=pd.concat([df2b, df3b], axis=1, join='inner')
+    #1136 subjects remain who have data in all columns
+    
+    data={}    
+    for i in dfc.index:
+        data[i]={}
+    
+    for i,j in zip(dfc.index, range(len(dfc.index))):
+        data[i]['sx_d007']=dfc['Day 7: HAMD Total'][j]
+        data[i]['sx_d300']=dfc['Day 300: HAMD Total'][j]
+        
+    for i in dfc.index:        
+        if data[i]['sx_d007']>=14: mdd=True
+        else: mdd=False
+        
+        if data[i]['sx_d300']<7 and mdd==True:
+            data[i]['tx_remitter']=True
+        else: data[i]['tx_remitter']=False
+        
+        if data[i]['sx_d300']<=(0.5*data[i]['sx_d007']) and mdd==True:
+            data[i]['tx_responder']=True
+        else: data[i]['tx_responder']=False
+        
+        if data[i]['sx_d300']>(0.5*data[i]['sx_d007']) and mdd==True:
+            data[i]['tx_nonresponder']=True
+        else: data[i]['tx_nonresponder']=False
+        
+    
+    #We want to restructure the shopvec code so we can just create columns in the existing "dfc" dataframe rather than making a secondary dict, "data". This is just to fit it into the existing shopvec code for now.
+    #data=pd.DataFrame(data)
+    
+    with open('/media/james/ext4data1/current/projects/pfizer/vectors/data'+'.pickle', 'wb') as d:
+        pickle.dump(data, d, pickle.HIGHEST_PROTOCOL) 
+    with open('/media/james/ext4data1/current/projects/pfizer/vectors/dfc'+'.pickle', 'wb') as d:
+        pickle.dump(dfc, d, pickle.HIGHEST_PROTOCOL) 
     return
 
-
 '''
+with open('/media/james/ext4data1/current/projects/pfizer/vectors/dfc.pickle', 'rb') as f:
+    dfc= pickle.load(f)
+with open('/media/james/ext4data1/current/projects/pfizer/vectors/data.pickle', 'rb') as f:
+    data= pickle.load(f)
+
+dfs2b['HAMD Total'].describe()
 np.intersect1d(dfs2['PATIENT'],dfs['PATIENT'])
 np.setdiff1d(dfs2['PATIENT'],dfs['PATIENT'])
 '''
