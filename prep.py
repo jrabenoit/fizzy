1e3ac1593a166ec3664f4d9d014d223deb06ab43
James Benoit <jrabenoit@gmail.com>
2017-11-05 19:47:31 -0700

    flattened discretes per subject

--- prep.py
+++ prep.py
@@ -1,16 +1,38 @@
 #!/usr/bin/env python3
 
 import pandas as pd
-import os
+import os, scipy.stats
 import numpy as np
 
 def Agglomerate():
-    for i in os.listdir('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csvtest'):
-        info=pd.read_csv('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csvtest/'+str(i), encoding='utf-8')
+    discrete= ['deid_adverse', 'deid_aemeddra', 'deid_cgi', 'deid_demow', 'deid_diaghist', 'deid_hamd17', 'deid_hamd17a', 'deid_labbase', 'deid_maddrs', 'deid_medhist', 'deid_medhist2', 'deid_nsmed', 'deid_othtrt', 'deid_physexam', 'deid_sdi', 'deid_stdymedw', 'deid_whowbi']
+
+   # continuous= [deid_demow, deid_ecgbase, deid_ecgtest, ded_labbase, deid_labtest, deid_qtcn, deid_qtcnbase, deid_stdymedw, deid_vas2, deid_vittest]
+    
+    for i in discrete:
+        info=pd.read_csv('/media/james/ext4data1/current/projects/pfizer/3151A1-303-csv/'+str(i)+'.csv', encoding='utf-8')
         a= info[info['CPENM']=='DAY 7']
-        b= info.set_index(['PATIENT'])
+        b= a.set_index(['PATIENT'])
         c= pd.get_dummies(b)
-        d= c.groupby(c.index).sum()
+        
+        d= {}
+        for i in list(set(c.index)):
+            d[i]= c.loc[i].values.flatten()
+        
+        maxlen=len(d[max(d, key=lambda k: len(d[k]))])    
+        
+        for i in d:
+            d[i]=np.append(d[i], [0]*(maxlen-len(d[i])))        
+        d= pd.DataFrame.from_dict(d, orient='index')
+        d.columns=list(c.columns)*scipy.stats.mode(c.index).count[0]
+        
+        #this gives a dataframe with all variables binarized
         
     return
+    
+    df.values.flatten
 
+
+    
+        
+        
