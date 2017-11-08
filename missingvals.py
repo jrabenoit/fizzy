2fa15f6af369a58cf147da6bd20c8f4b29872fa5
James Benoit <jrabenoit@gmail.com>
2017-11-08 16:45:55 -0700

    dealing with sparseness

---
+++ missingvals.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import os, scipy.stats
+import numpy as np
+from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import MaxAbsScaler
+
+#http://scikit-learn.org/stable/modules/preprocessing.html#imputation-of-missing-values
+
+def Impute():
+    '''Imputes missing vals by mean, scales as sparse matrix, 96% sparse after scaling'''
+    info=pd.read_csv('/media/james/ext4data1/current/projects/pfizer/final_vecs.csv', encoding='utf-8')
+    info=info.set_index('PATIENT')
+    
+    #tested axes- we want axis 0 to impute mean down a column
+    imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+    X= imp.fit_transform(info)
+    
+    #using a sparse data scaler due to the number of zeros from binarized variables
+    mas= MaxAbsScaler()
+    X= mas.fit_transform(X)
+    #str((np.count_nonzero(X)/(X.shape[0]*X.shape[1]))*100) to get density... about 4% atm
+    
+    
+    #Should return to the psych scales (madrs, hamd etc) and encode using OneHotEncoder, since continuous variables are expected.
+    
+    np.savetxt('/media/james/ext4data1/current/projects/pfizer/ready.csv', X, delimiter=',')
+    
+    return
